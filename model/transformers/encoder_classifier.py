78c86ead44d6a97b8c656369db482065dfbf6836
Pratyush-exe <pratyush21225@gmail.com>
2023-11-26 18:43:37 +0530

    FEAT: Added EncoderClassifier model to test out model sanity

---
+++ model/transformers/encoder_classifier.py
@@ -0,0 +1,27 @@
+from torch import nn
+from model.embeddings import Embedding
+from model import Encoder
+
+class EncoderClassifier(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        
+        self.config = config
+        self.embed_layer = Embedding(config)
+        self.encoder = Encoder(config)
+        embed_dims = config['embed_dims']
+        
+        self.classifier_head = nn.Sequential(
+            nn.Linear(embed_dims, embed_dims),
+            nn.ReLU(),
+            nn.Dropout(config['dropout']),
+            nn.Linear(embed_dims, config['num_classes']),
+            nn.Softmax(dim=1),
+        )
+        
+    def forward(self, x):
+        output = self.embed_layer(x)
+        output = self.encoder(output)
+        output = self.classifier_head(output)
+        
+        return output 
